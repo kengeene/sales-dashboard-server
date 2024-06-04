e4aaab556a8816eb85d60a67ef43394e19e4003c
enkuria <kengeene@gmail.com>
2024-06-04 14:16:12 +0300

    initial commit

---
+++ api/server.js
@@ -0,0 +1,23 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("db.json");
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running on port 3000");
+});
+
+// Export the Server API
+module.exports = server;
